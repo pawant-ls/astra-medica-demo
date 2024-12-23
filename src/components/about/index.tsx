ca1b33a51ac9a6eec641956e6acd56e8e9f72a6c
pawant-ls <pawantamada@luciensolutions.com>
2024-12-23 14:58:09 +0530

    resolved build errors

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,13 +1,22 @@
+"use client"
+
 import About from "../home/about";
+import OurTeam from "../home/team";
 import SecondaryPageHero from "../secondary-hero";
+import ResearchTopic from "./research-topic";
+import TestimonialsAbout from "./testimonials-about";
 
 function AboutComponent() {
   return (
     <main>
       <SecondaryPageHero heroText="About astra medica" />
       <div className="-mt-[20vh]">
-      <About />
+        <About />
       </div>
+      <ResearchTopic />
+      <TestimonialsAbout />
+
+      <OurTeam />
     </main>
   );
 }
---
+++ src/components/about/research-topic.tsx
@@ -0,0 +1,14 @@
+function ResearchTopic() {
+  return (
+    <section className="flex flex-col py-40 items-center justify-center space-y-4 bg-gradient-to-r from-primary/15 via-primary/25 to-primary/60">
+      <h6 className="font-heading font-semibold text-md uppercase text-primary">
+        Research Topic
+      </h6>
+      <h2 className="font-heading font-bold text-center text-black text-4xl">
+        Medical, Medicine and Health Science Topic
+      </h2>
+    </section>
+  );
+}
+
+export default ResearchTopic;
